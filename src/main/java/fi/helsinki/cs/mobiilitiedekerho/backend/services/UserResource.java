97a0f56ce1491890cc920fdd0bfef9962d91659a
L4K0NL98JB <juhanasaarinen@tunturipaju.local>
2016-02-02 20:05:29 +0200

    Create classes UserService and UserResource.

--- src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/services/UserResource.java
+++ src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/services/UserResource.java
@@ -1,5 +1,87 @@
 package fi.helsinki.cs.mobiilitiedekerho.backend.services;
 
+import fi.helsinki.cs.mobiilitiedekerho.backend.models.User;
+
+import spark.Spark;
+import spark.Response;
+import spark.Request;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+
+import java.util.List;
+import java.lang.Integer;
+
 public class UserResource {
-    
+
+    private final UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+
+        defineRoutes();
+    }
+
+    private void setContentTypeToJson(Response res) {
+        res.type("application/json");
+    }
+
+    private void defineRoutes() {
+        Spark.get("/DescribeUser", (req, res) -> {
+            setContentTypeToJson(res);
+            return this.describeUser(req, res);
+        });
+
+        Spark.get("/AuthenticateUser", (req, res) -> {
+            setContentTypeToJson(res);
+            return this.authenticateUser(req, res);
+        });
+    }
+
+    private String describeUser(Request req, Response res) {
+        String user_id = req.queryParams("user_id");
+        JsonResponse jsonResponse = new JsonResponse();
+
+        if (user_id == null) {
+            jsonResponse.setStatus("ParameterError");
+            return jsonResponse.toJson();
+        }
+
+        User user = userService.getUserById(Integer.parseInt(user_id));
+
+        if (user == null) {
+            jsonResponse.setStatus("UserNotFoundError");
+            return jsonResponse.toJson();
+        }
+
+        jsonResponse.setObject(user);
+
+        return jsonResponse.setStatus("Success").toJson();
+    }
+
+    private String authenticateUser(Request req, Response res) {
+        String email = req.queryParams("email");
+        String password = req.queryParams("password");
+        JsonResponse jsonResponse = new JsonResponse();
+        
+        if (email == null) {
+            jsonResponse.setStatus("ParameterError");
+            return jsonResponse.toJson();
+        }
+        
+        if (password == null) {
+            jsonResponse.setStatus("ParameterError");
+            return jsonResponse.toJson();
+        }    
+
+        User user = this.userService.authenticateUser(email, password);
+
+        if (user == null) {
+            return new JsonResponse().setStatus("AuthFailure").toJson();
+        } else {
+            this.userService.createAuthHashForUser(user.getId());
+            user = this.userService.getUserById(user.getId());
+            return new JsonResponse().setObject(user).setStatus("Success").toJson();
+        }
+    }
 }
--- src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/services/UserService.java
+++ src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/services/UserService.java
@@ -1,5 +1,110 @@
 package fi.helsinki.cs.mobiilitiedekerho.backend.services;
 
+import fi.helsinki.cs.mobiilitiedekerho.backend.models.User;
+
+import org.sql2o.*;
+
+import java.util.List;
+import java.security.MessageDigest;
+import java.util.Arrays;
+
 public class UserService {
-    
+
+    private final Sql2o sql2o;
+
+    public UserService(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    public User getUserById(int user_id) {
+        String sql
+                = "SELECT *"
+                + "FROM user "
+                + "WHERE id = :id";
+
+        try (Connection con = sql2o.open()) {
+            List<User> users = con.createQuery(sql)
+                    .addParameter("id", user_id)
+                    .executeAndFetch(User.class);
+            
+            if (users.isEmpty()) {
+                return null;
+            } else {
+                return users.get(0);
+            }
+        }
+    }
+
+    public User authenticateUser(String email, String password) {
+        String sql
+                = "SELECT *"
+                + "FROM user "
+                + "WHERE email = :email AND password = :password";
+
+        try (Connection con = sql2o.open()) {
+            List<User> users = con.createQuery(sql)
+                    .addParameter("email", email)
+                    .addParameter("password", password)
+                    .executeAndFetch(User.class);
+
+            if (users.isEmpty()) {
+                return null;
+            } else {
+                return users.get(0);
+            }
+        }
+    }
+
+    public User authenticateUserByHash(String user_hash) {
+        String sql
+                = "SELECT *"
+                + "FROM user "
+                + "WHERE hash = :hash";
+
+        try (Connection con = sql2o.open()) {
+            List<User> users = con.createQuery(sql)
+                    .addParameter("hash", user_hash)
+                    .executeAndFetch(User.class);
+
+            if (users.isEmpty()) {
+                return null;
+            } else {
+                return users.get(0);
+            }
+        }
+    }
+
+    public boolean createAuthHashForUser(int user_id) {
+
+        // Generate a new random sha-256 hash to be stored
+        // in the user database.
+        java.util.Random random = new java.util.Random();
+        int random_integer = random.nextInt();
+        String new_hash = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(Integer.toString(random_integer).getBytes());
+            new_hash = Arrays.toString(md.digest());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        String sql
+                = "UPDATE user"
+                + "SET hash = :hash "
+                + "WHERE id = :id";
+
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("hash", new_hash)
+                    .addParameter("id", user_id)
+                    .executeUpdate();
+        }
+
+        return true;
+    }
+
+    public List<User> getAllUsers(int user_id) {
+        return java.util.Collections.emptyList();
+    }
 }
