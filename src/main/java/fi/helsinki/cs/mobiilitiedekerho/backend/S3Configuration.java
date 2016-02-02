bad5dbbf67e2906cd6646e353a6cfd84e593132d
L4K0NL98JB <juhanasaarinen@tunturipaju.local>
2016-02-02 15:37:27 +0200

    Update S3 url and bucket names.

--- src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/S3Configuration.java
+++ src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/S3Configuration.java
@@ -1,7 +1,7 @@
 package fi.helsinki.cs.mobiilitiedekerho.backend;
 
 public class S3Configuration {
-    public static final String S3_URL = "";
-    public static final String S3_ANSWER_BUCKET_NAME = "";
-    public static final String S3_TASK_BUCKET_NAME = "";
+    public static final String S3_URL = "https://s3.eu-central-1.amazonaws.com/";
+    public static final String S3_ANSWER_BUCKET_NAME = "p60v4ow30312-answers";
+    public static final String S3_TASK_BUCKET_NAME = "p60v4ow30312-tasks";
 }
