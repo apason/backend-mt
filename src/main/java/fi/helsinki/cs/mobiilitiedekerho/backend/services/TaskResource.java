852b70c4177aef06951169def60d39fdb7a80f98
L4K0NL98JB <juhanasaarinen@tunturipaju.local>
2016-02-03 09:55:48 +0200

    Refactoring. Removed unused imports.

--- src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/services/TaskResource.java
+++ src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/services/TaskResource.java
@@ -1,57 +1,55 @@
 package fi.helsinki.cs.mobiilitiedekerho.backend.services;
+
 import fi.helsinki.cs.mobiilitiedekerho.backend.models.Task;
 
 import spark.Spark;
 import spark.Response;
 import spark.Request;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-
 import java.util.List;
-import java.lang.Integer;
 
 public class TaskResource {
-  private final TaskService taskService;
 
-  public TaskResource(TaskService taskService) {
-    this.taskService = taskService;
-    
-    setContentType();
-    
-    defineRoutes();
-  }
-  
-  private void setContentType() {
-      Spark.after((req, res) -> {
-          res.type("application/json");
-      });
-  }
+    private final TaskService taskService;
 
-  private void defineRoutes() {
-    Spark.get("/DescribeTask", (req, res) -> {
-        return this.describeTask(req, res);
-    });
-  }
-  
-  private String describeTask(Request req, Response res) {
-      String task_id = req.queryParams("task_id");
-      JsonResponse jsonResponse = new JsonResponse();
-      
-      if (task_id == null) {
-          jsonResponse.setStatus("ParameterError");
-          return jsonResponse.toJson();
-      }
+    public TaskResource(TaskService taskService) {
+        this.taskService = taskService;
 
-      List<Task> tasks = taskService.getTaskById(Integer.parseInt(task_id));
-      
-      if (tasks.isEmpty()) {
-          jsonResponse.setStatus("TaskNotFoundError");
-          return jsonResponse.toJson();
-      }
-      
-      jsonResponse.setObject(tasks.get(0));
-      
-      return jsonResponse.setStatus("Success").toJson();
-  }
+        setContentType();
+
+        defineRoutes();
+    }
+
+    private void setContentType() {
+        Spark.after((req, res) -> {
+            res.type("application/json");
+        });
+    }
+
+    private void defineRoutes() {
+        Spark.get("/DescribeTask", (req, res) -> {
+            return this.describeTask(req, res);
+        });
+    }
+
+    private String describeTask(Request req, Response res) {
+        String task_id = req.queryParams("task_id");
+        JsonResponse jsonResponse = new JsonResponse();
+
+        if (task_id == null) {
+            jsonResponse.setStatus("ParameterError");
+            return jsonResponse.toJson();
+        }
+
+        List<Task> tasks = taskService.getTaskById(Integer.parseInt(task_id));
+
+        if (tasks.isEmpty()) {
+            jsonResponse.setStatus("TaskNotFoundError");
+            return jsonResponse.toJson();
+        }
+
+        jsonResponse.setObject(tasks.get(0));
+
+        return jsonResponse.setStatus("Success").toJson();
+    }
 }
