ff0c4adc96c0df0a9bb16cedc787e0b691fea593
L4K0NL98JB <juhanasaarinen@tunturipaju.local>
2016-02-16 13:19:56 +0200

    Refactoring.

---
+++ src/test/java/fi/helsinki/cs/mobiilitiedekerho/backend/services/AnswerResourceTest.java
@@ -0,0 +1,81 @@
+package fi.helsinki.cs.mobiilitiedekerho.backend.services;
+
+import fi.helsinki.cs.mobiilitiedekerho.backend.models.*;
+
+import spark.Request;
+import spark.Response;
+
+import static junit.framework.Assert.assertTrue;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.util.Optional;
+import java.util.Date;
+
+import static org.mockito.Mockito.*;
+
+public class AnswerResourceTest extends TestCase {
+    
+    private UserService userService;
+    private AnswerService answerService;
+    private AnswerResource answerResource;
+    
+    private Request req;
+    private Response res;
+    
+
+    public AnswerResourceTest(String testName)
+    {
+        super(testName);
+    }
+
+    public static Test suite()
+    {
+        return new TestSuite(AnswerResourceTest.class);
+    }
+    
+    protected void setUp() {
+        answerService = mock(AnswerService.class);
+        userService = mock(UserService.class);
+        
+        req = mock(Request.class);
+        res = mock(Response.class);       
+
+        Answer answer = new Answer();
+        answer.setId(1);
+        answer.setUser_id(1);
+        answer.setTask_id(1);
+        answer.setUri("video.mp4");
+        answer.setIssued(new Date(0));
+        answer.setLoaded(new Date(0));
+        
+        when(answerService.getAnswerById(1)).thenReturn(Optional.of(answer));
+        when(answerService.getAnswerById(2)).thenReturn(Optional.empty());
+        
+        answerResource = new AnswerResource(userService, answerService);
+    }
+
+    public void testDescribeAnswerkSuccess() {         
+        when(req.queryParams("answer_id")).thenReturn("1"); 
+        
+        String jsonResponse = answerResource.describeAnswer(req, res);
+        
+        String jsonExpected = "";
+        
+        // assertEquals(jsonResponse, jsonExpected);
+        
+        assertTrue(true);
+    }
+
+    public void testDescribeAnswerkNotFound() {         
+        when(req.queryParams("answer_id")).thenReturn("2"); 
+        
+        String jsonResponse = answerResource.describeAnswer(req, res);
+        
+        String jsonExpected = "{\"status\":\"AnswerNotFound\"}";
+        // assertEquals(jsonResponse, jsonExpected);
+        
+        assertTrue(true);
+    }
+}
