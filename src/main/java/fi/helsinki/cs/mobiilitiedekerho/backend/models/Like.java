921349c82e8475b8bd7e1a5d899f2a769eaadb44
Daniel <daniel.martinkarttunen@helsinki.fi>
2016-03-25 01:51:51 +0200

    Created and done Like's model.

---
+++ src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/models/Like.java
@@ -0,0 +1,59 @@
+package fi.helsinki.cs.mobiilitiedekerho.backend.models;
+
+import java.util.Date;
+
+
+public class Like {
+
+    private int id;
+    private String likeType; //E.G "task" for a task-like and "answer" for a answer-like. If only those two then a boolean would be enought (though there may be more).
+    private int liked_id;
+    private int subuser_id;
+    private Date loaded;
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+    public String getlikeType() {
+        return likeType;
+    }
+
+    public void setlikeType(String likeType) {
+        this.likeType = likeType;
+    }
+    
+
+    public int getLiked_id() {
+        return liked_id;
+    }
+
+    public void setLike_id(int liked_id) {
+        this.liked_id = liked_id;
+    }
+    
+
+    public int getSubUserId() {
+        return subuser_id;
+    }
+
+    public void setSubUserId(int subuser_id) {
+        this.subuser_id = subuser_id;
+    }
+    
+    
+    public Date getLoaded() {
+        return loaded;
+    }
+
+    public void setLoaded(Date loaded) {
+        this.loaded = loaded;
+    }
+    
+}
