babd85e90346d40ca12b77b67dc23cc7d357cdb1
Daniel <daniel.martinkarttunen@helsinki.fi>
2016-04-01 00:28:28 +0300

    Renamed Subuser model's filename to match its class-name and updated it to contain avatar_url.

--- src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/models/Subuser.java
+++ src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/models/Subuser.java
@@ -8,6 +8,7 @@ public class Subuser {
     private int user_id;
     private Date created;
     private String nick;
+    private String avatar_url;
 
     public int getId() {
         return id;
@@ -39,5 +40,13 @@ public class Subuser {
 
     public void setNick(String nick) {
         this.nick = nick;
+    }
+    
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
     }
 }
