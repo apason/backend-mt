1df956ac0809f300513d05e6f345726e1a0d85e9
L4K0NL98JB <juhanasaarinen@tunturipaju.local>
2016-04-20 22:22:00 +0300

    Move configuration to application.conf. Load configuration in main method.

--- src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/App.java
+++ src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/App.java
@@ -10,49 +10,58 @@ import spark.Spark;
 import java.security.Key;
 import io.jsonwebtoken.impl.crypto.MacProvider;
 
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.Config;
+
 public class App {
 
     // Backend initialization.
     public static void main(String[] args) {
+        SparkConfiguration();
+        
+        // Load application-wide confguration from application.json 
+        Config appConfiguration = ConfigFactory.load();
+        
         // Sql2o object for the services
         Sql2o sql2o = new Sql2o(App.configureHikariConnectionPool());
         
         // Generates a secret key for JSON Web Token signatures.
         Key key = MacProvider.generateKey();
-
-        SparkConfiguration();
-
-        // Start the services.
-        UserService userService = new UserService(sql2o, key);
         
+        UserService userService = new UserService(sql2o, key);        
         TaskService taskService = new TaskService(sql2o);
         AnswerService answerService = new AnswerService(sql2o, userService);
         CategoryService categoryService = new CategoryService(sql2o);
         LikeService likeService = new LikeService(sql2o);
 
+
         
         //This class is 'both' resource and service. 
-        Misc misc = new Misc(userService, sql2o);
+        Misc misc = new Misc(userService, sql2o, appConfiguration);
 
         //Start resources.
-        LikeResource likeResource = new LikeResource(likeService, userService, answerService);
-        TaskResource taskResource = new TaskResource(userService, taskService);
-        AnswerResource answerResource = new AnswerResource(userService, answerService);
-        UserResource userResource = new UserResource(userService);
-        CategoryResource categoryResource = new CategoryResource(userService, categoryService);
-
+        LikeResource likeResource = new LikeResource(likeService, userService, answerService, appConfiguration);
+        TaskResource taskResource = new TaskResource(userService, taskService, appConfiguration);
+        AnswerResource answerResource = new AnswerResource(userService, answerService, appConfiguration);
+        UserResource userResource = new UserResource(userService, appConfiguration);
+        CategoryResource categoryResource = new CategoryResource(userService, categoryService, appConfiguration);
     }
 
     // Configures Hikari Connection Pool.
     private static HikariDataSource configureHikariConnectionPool() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(DbConfiguration.DB_JDBC_URL);
-        config.setUsername(DbConfiguration.DB_USERNAME);
-        config.setPassword(DbConfiguration.DB_PASSWORD);
-        config.addDataSourceProperty("cachePrepStmts", "true");
-        config.addDataSourceProperty("prepStmtCacheSize", "250");
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-        return new HikariDataSource(config);
+        // Load application-wide confguration from application.json
+        Config appConf = ConfigFactory.load();
+        
+        HikariConfig hikariConfig = new HikariConfig();
+        
+        hikariConfig.setJdbcUrl(appConf.getString("db.jdbc_"));
+        hikariConfig.setUsername(appConf.getString("db.username"));
+        hikariConfig.setPassword(appConf.getString("db.password"));
+        hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
+        hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+        hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        
+        return new HikariDataSource(hikariConfig);
     }
     
     // Sets response type to application/json.
