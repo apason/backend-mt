ef6b0966d19310a0a37c128e00a85296325f6934
L4K0NL98JB <juhanasaarinen@tunturipaju.local>
2016-04-21 14:25:52 +0300

    Remove unused class S3Configuration. All configuration is now in application.conf.

--- src/main/java/fi/helsinki/cs/mobiilitiedekerho/backend/S3Configuration.java
+++
@@ -1,8 +0,0 @@
-package fi.helsinki.cs.mobiilitiedekerho.backend;
-
-public class S3Configuration {
-    public static final String S3_URL = "https://s3.eu-central-1.amazonaws.com/";
-    public static final String S3_ANSWER_BUCKET_NAME = "mobiilitiedekerho-answers";
-    public static final String S3_TASK_BUCKET_NAME = "mobiilitiedekerho-tasks";
-    public static final String S3_GRAPHICS_BUCKET_NAME = "mobiilitiedekerho-graphics";
-}
